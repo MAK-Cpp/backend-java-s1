4c416674ab7075a9644baf43581ed6621ad55bc1
MAK-Cpp <spartmenik@gmail.com>
2023-11-16 17:11:43 +0300

    simplified task6 solution

--- src/main/java/edu/hw5/tasks/task6/SubstringInString.java
+++ src/main/java/edu/hw5/tasks/task6/SubstringInString.java
@@ -1,38 +1,13 @@
 package edu.hw5.tasks.task6;
 
-import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public final class SubstringInString {
-    private static final Map<Character, String> SPECIAL_CHARACTERS = Map.of(
-        '\n', "\\n",
-        '\t', "\\t",
-        '\r', "\\r",
-        '\f', "\\f",
-        '\s', "\\s",
-        '\b', "\\b"
-    );
-    private static final Set<Character> REGEX_CHARACTERS =
-        Set.of('<', '(', '[', '{', '\\', '^', '-', '=', '$', '!', '|', ']', '}', ')', '?', '*', '+', '.', '>');
-
     private SubstringInString() {
     }
 
     public static boolean check(final String string, final String text) {
-        final Pattern substringPattern = Pattern.compile(string.chars()
-            .mapToObj(x -> {
-                Character charX = (char) x;
-                if (SPECIAL_CHARACTERS.containsKey(charX)) {
-                    return SPECIAL_CHARACTERS.get(charX);
-                } else if (REGEX_CHARACTERS.contains(charX)) {
-                    return "\\" + charX;
-                } else {
-                    return charX.toString();
-                }
-            })
-            .collect(Collectors.joining()));
+        final Pattern substringPattern = Pattern.compile(Pattern.quote(string));
         return substringPattern.matcher(text).find();
     }
 }

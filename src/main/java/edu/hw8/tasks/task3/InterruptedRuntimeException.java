db506a38e31dc35732d037693c84e958a520d168
MAK-Cpp <spartmenik@gmail.com>
2023-12-06 16:32:06 +0300

    fixed InterruptedRuntimeException

--- src/main/java/edu/hw8/tasks/task3/InterruptedRuntimeException.java
+++ src/main/java/edu/hw8/tasks/task3/InterruptedRuntimeException.java
@@ -1,18 +1,11 @@
 package edu.hw8.tasks.task3;
 
 public class InterruptedRuntimeException extends RuntimeException {
-    public InterruptedRuntimeException() {
-    }
-
-    public InterruptedRuntimeException(String message) {
-        super(message);
-    }
-
-    public InterruptedRuntimeException(String message, Throwable cause) {
+    public InterruptedRuntimeException(String message, InterruptedException cause) {
         super(message, cause);
     }
 
-    public InterruptedRuntimeException(Throwable cause) {
+    public InterruptedRuntimeException(InterruptedException cause) {
         super(cause);
     }
 }

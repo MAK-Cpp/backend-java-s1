d4fb6fc307625911f4e53e52ad0cc09825d229c3
Maxim Primakov <spartmenik@gmail.com>
2023-12-17 19:55:25 +0300

    written task No.1

---
+++ src/test/java/edu/hw11/tasks/task1/HelloByteBuddyTest.java
@@ -0,0 +1,24 @@
+package edu.hw11.tasks.task1;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.InvocationTargetException;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloByteBuddyTest {
+    @Test
+    void testHelloByteBuddy()
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<?> helloClass = new ByteBuddy()
+            .subclass(Object.class)
+            .method(named("toString"))
+            .intercept(FixedValue.value("Hello, ByteBuddy"))
+            .make()
+            .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+            .getLoaded();
+        assertThat(helloClass.getDeclaredConstructor().newInstance().toString()).isEqualTo("Hello, ByteBuddy");
+    }
+}

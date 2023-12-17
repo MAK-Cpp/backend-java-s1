7b832f261291aa2bfdb96cd4d6806427efad1b9a
Maxim Primakov <spartmenik@gmail.com>
2023-12-17 20:38:18 +0300

    written task No.2

--- src/test/java/edu/hw11/tasks/task1/HelloByteBuddyTest.java
+++ src/test/java/edu/hw11/tasks/task1/HelloByteBuddyTest.java
@@ -3,11 +3,13 @@ package edu.hw11.tasks.task1;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.FixedValue;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.InvocationTargetException;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Task 1")
 public class HelloByteBuddyTest {
     @Test
     void testHelloByteBuddy()
---
+++ src/test/java/edu/hw11/tasks/task2/ReplaceMethodTest.java
@@ -0,0 +1,48 @@
+package edu.hw11.tasks.task2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Task 2")
+public class ReplaceMethodTest {
+    public static class Source {
+        public String hello(String name) { return null; }
+    }
+
+    public static class Target {
+        public static String hi(String name) {
+            return "Hello " + name + "!";
+        }
+    }
+
+    public static Stream<Arguments> testReplaceMethod() {
+        return Stream.of(
+            Arguments.of(
+                Source.class,
+                "hello",
+                Target.class,
+                new Object[] {"Maxim"},
+                "Hello Maxim!"
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testReplaceMethod(Class<?> origin, String methodName, Class<?> replacing, Object[] args, Object output)
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<?> replaced = ReplaceMethod.replace(origin, methodName, replacing);
+        Class<?>[] argsClasses = new Class<?>[args.length];
+        for (int i = 0; i < args.length; i++) {
+            argsClasses[i] = args[i].getClass();
+        }
+        Method method = replaced.getMethod(methodName, argsClasses);
+        assertThat(method.invoke(replaced.getDeclaredConstructor().newInstance(), args)).isEqualTo(output);
+    }
+}

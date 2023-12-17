261c7ad23f83f119b6f6932df8e738261125f623
Maxim Primakov <spartmenik@gmail.com>
2023-12-17 23:58:09 +0300

    written project5

---
+++ src/main/java/edu/project5/ReflectionBenchmark.java
@@ -0,0 +1,99 @@
+package edu.project5;
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.LambdaConversionException;
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+@State(Scope.Thread)
+public class ReflectionBenchmark {
+    @SuppressWarnings({"checkstyle:UncommentedMain", "checkstyle:MagicNumber"})
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+            .include(ReflectionBenchmark.class.getSimpleName())
+            .shouldFailOnError(true)
+            .shouldDoGC(true)
+            .mode(Mode.AverageTime)
+            .timeUnit(TimeUnit.NANOSECONDS)
+            .forks(1)
+            .warmupForks(1)
+            .warmupIterations(1)
+            .warmupTime(TimeValue.seconds(5))
+            .measurementIterations(1)
+            .measurementTime(TimeValue.seconds(5))
+            .build();
+
+        new Runner(options).run();
+    }
+
+    private Student student;
+    private Method method;
+    private MethodHandle methodHandle;
+    private Name funcInterface;
+
+    @Setup
+    public void setup() throws Throwable {
+        student = new Student("Alexander", "Biryukov");
+        method = Student.class.getMethod("name");
+        MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
+        MethodType methodType = MethodType.methodType(String.class);
+        methodHandle = publicLookup.findVirtual(student.getClass(), "name", methodType);
+        CallSite callSite = LambdaMetafactory.metafactory(
+            publicLookup,
+            "name",
+            MethodType.methodType(Name.class, Student.class),
+            methodType,
+            methodHandle,
+            methodType
+        );
+        funcInterface = (Name) callSite.getTarget().bindTo(student).invoke();
+    }
+
+    @Benchmark
+    public void directAccess(Blackhole bh) {
+        String name = student.name();
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void reflection(Blackhole bh) throws InvocationTargetException, IllegalAccessException {
+        Object name = method.invoke(student);
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void methodHandle(Blackhole bh) throws Throwable {
+        Object name = methodHandle.invoke(student);
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void lambdaMetaFactory(Blackhole bh) throws Throwable {
+        String name = funcInterface.get();
+        bh.consume(name);
+    }
+
+    public record Student(String name, String surname) {
+    }
+
+    @FunctionalInterface
+    public interface Name {
+        String get();
+    }
+}

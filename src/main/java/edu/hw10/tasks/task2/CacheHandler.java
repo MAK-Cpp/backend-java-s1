36893a62bd51cd82095290149bea303ce8b9b79e
Maxim Primakov <spartmenik@gmail.com>
2023-12-19 01:40:27 +0300

    written loading persistent cache when create CacheHandler

--- src/main/java/edu/hw10/tasks/task2/CacheHandler.java
+++ src/main/java/edu/hw10/tasks/task2/CacheHandler.java
@@ -1,7 +1,9 @@
 package edu.hw10.tasks.task2;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationHandler;
@@ -11,13 +13,41 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class CacheHandler implements InvocationHandler {
     private final HashMap<Map.Entry<Method, Object[]>, Object> cachedValues = new HashMap<>();
     private final Object object;
+    private final Path objectDirectory;
 
     public CacheHandler(Object object) {
         this.object = object;
+        this.objectDirectory =
+            Path.of(System.getProperty("user.dir")).resolve(".cached").resolve(object.getClass().getName());
+        if (Files.exists(objectDirectory)) {
+            for (Method method : object.getClass().getMethods()) {
+                Path directory = objectDirectory.resolve(method.getName());
+                if (!Files.exists(directory)) {
+                    continue;
+                }
+                Object result;
+                Object[] arguments;
+                int argumentsCount;
+                for (File file : Objects.requireNonNull(directory.toFile().listFiles())) {
+                    try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+                        result = objectInputStream.readObject();
+                        argumentsCount = objectInputStream.readInt();
+                        arguments = new Object[argumentsCount];
+                        for (int i = 0; i < argumentsCount; i++) {
+                            arguments[i] = objectInputStream.readObject();
+                        }
+                        cachedValues.put(Map.entry(method, arguments), result);
+                    } catch (IOException | ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -29,9 +59,7 @@ public class CacheHandler implements InvocationHandler {
         Object result;
         if (cache.persist()) {
             String key = String.join(";", Arrays.stream(args).map(Object::toString).toArray(String[]::new));
-            Path directory =
-                Path.of(System.getProperty("user.dir")).resolve(".cached").resolve(object.getClass().getName())
-                    .resolve(method.getName());
+            Path directory = objectDirectory.resolve(method.getName());
             if (!Files.exists(directory)) {
                 Files.createDirectories(directory);
             }
@@ -43,6 +71,10 @@ public class CacheHandler implements InvocationHandler {
                     ObjectOutputStream objectOutputStream =
                         new ObjectOutputStream(new FileOutputStream(file.toFile()))) {
                     objectOutputStream.writeObject(result);
+                    objectOutputStream.writeInt(args.length);
+                    for (Object arg : args) {
+                        objectOutputStream.writeObject(arg);
+                    }
                 }
             } else {
                 try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file.toFile()))) {

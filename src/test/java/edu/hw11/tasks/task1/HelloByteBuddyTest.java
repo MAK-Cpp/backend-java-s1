97a94796c03aa7bf1e45e109270dc3c179c0add1
Maxim Primakov <spartmenik@gmail.com>
2023-12-22 15:16:59 +0300

    replaced `named("toString")` to `ElementMatchers.isToString()`

--- src/test/java/edu/hw11/tasks/task1/HelloByteBuddyTest.java
+++ src/test/java/edu/hw11/tasks/task1/HelloByteBuddyTest.java
@@ -3,6 +3,7 @@ package edu.hw11.tasks.task1;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.InvocationTargetException;
@@ -16,7 +17,7 @@ public class HelloByteBuddyTest {
         throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> helloClass = new ByteBuddy()
             .subclass(Object.class)
-            .method(named("toString"))
+            .method(ElementMatchers.isToString())
             .intercept(FixedValue.value("Hello, ByteBuddy"))
             .make()
             .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
